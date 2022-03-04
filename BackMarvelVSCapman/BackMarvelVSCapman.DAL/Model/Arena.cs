90a13cb9015012f8ca9a0f9aa65341c450e15705
vietdamien <damsng63@gmail.com>
2022-03-04 14:00:23 +0100

    Add arena, associated DTO and repository (controller has yet to be done)

---
+++ BackMarvelVSCapman/BackMarvelVSCapman.DAL/Model/Arena.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackMarvelVSCapman.DAL.Model
+{
+    public class Arena
+    {
+        [Key]
+        public int ArenaId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Required]
+        public string Image { get; set; } = "";
+    }
+}
